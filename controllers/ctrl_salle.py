b862a68cbf4731f21279acaac91988c99fc77778
Vincent Pitard <vpitard@sio-coliniere44.local>
2016-03-22 14:50:05 +0100

    Issue A1

--- controllers/ctrl_salle.py
+++ controllers/ctrl_salle.py
@@ -6,8 +6,7 @@ def afficherLesSalles():
     """
     Fournit à la vue HTML la liste de toutes les salles avec l'ensemble des informations
     """
-    rowsSalles =db(db.salle.categorie_id==db.categorie.id).select(db.salle.id,db.salle.nom,db.categorie.nom) #requête permettant de récupérer les informations des salles
-
+    rowsSalles =db(db.salle.categorie_id==db.categorie.id).select(db.salle.id,db.salle.nom,db.categorie.nom,db.salle.capacite,db.categorie.heureOuverture,db.categorie.heureOuvertureMinutes,db.categorie.heureFermeture,db.categorie.heureFermetureMinutes, orderby=db.categorie.nom|db.salle.capacite) #requête permettant de récupérer les informations des salles
     return locals()
 
 #------------------------------------------------------------------------------------#
